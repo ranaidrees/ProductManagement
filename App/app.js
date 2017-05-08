c5bac531f5c15cb23375efc417a7fedce0c977bf
Rana Idrees <naveed_idrees@yahoo.com>
2017-05-08 16:54:20 +0100

    navigation impplemented

--- App/app.js
+++ App/app.js
@@ -4,13 +4,25 @@
 
     app.config(["$stateProvider","$urlRouterProvider",
         function ($stateProvider, $urlRouterProvider) {
-          //   $urlRouterProvider.otherwise('/');
-            $stateProvider.state("products",
-            {
-                url: "/products",
-                templateUrl: "app/products/productListView.html",
-                controller: "ProductListCtrl as vm"
-            });
+             $urlRouterProvider.otherwise("/products");
+            $stateProvider
+                .state("home",
+                {
+                    url: "/",
+                    templateUrl: "app/welcomeView.html"
+                })
+                .state("productList",
+                {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",
+                    controller: "ProductListCtrl as vm"
+                })
+                .state("productEdit",
+                {
+                    url: "/prdoucts/edit/:productId",
+                    templateUrl: "app/products/productEditView.html",
+                    controller: "ProductEditCtrl as vm"
+                });
         }]
 
         );
